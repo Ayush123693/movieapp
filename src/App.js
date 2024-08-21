919467945cbc9c73e1096651295197a031219806
Ayush Singh <singhayushiit2018@gmail.com>
2024-08-21 23:36:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
@@ -10,8 +11,8 @@ const App = () => {
       <header>
         <nav>
           <ul>
-            <li><a href="#" onClick={() => console.log("Home clicked")}>Home</a></li>
-            <li><a href="#" onClick={() => console.log("Favorites clicked")}>Favorites</a></li>
+            <li><a onClick={() => console.log("Home clicked")}>Home</a></li>
+            <li><a onClick={() => console.log("Favorites clicked")}>Favorites</a></li>
           </ul>
         </nav>
       </header>
